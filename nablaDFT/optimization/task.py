4d668180f9b34d34665b6a14e948250074b0a5e3
Anton Ber <ber@airi.net>
2024-02-26 19:40:52 +0300

    initial commit for optim task

---
+++ nablaDFT/optimization/task.py
@@ -0,0 +1,26 @@
+from ase.db import connect
+
+
+class BatchwiseOptimizeTask:
+    def __init__(self) -> None:
+        pass
+        
+        self.calculator = None
+        self.optimizer = None
+        self.converter = None
+        self.model = None
+        self.bs = None
+        self.data_db_conn = None
+        self.out_db_conn = None
+
+    def optimize_batch(self):
+        pass
+
+    def run(self):
+        pass
+
+    def _open(self):
+        pass
+
+    def _write_preds(self):
+        pass
