9ce57d35a8531b86eac8e705f05f803d10acc474
BerAnton <ber@airi.net>
2024-03-22 11:54:52 +0300

    updated hamiltonian links

--- nablaDFT/dataset/__init__.py
+++ nablaDFT/dataset/__init__.py
@@ -1,3 +1,3 @@
 from .nablaDFT_dataset import ASENablaDFT, PyGNablaDFTDataModule, PyGHamiltonianDataModule
-from .hamiltonian_dataset import HamiltonianDataset  # database interface for Hamiltonian datasets
+from .hamiltonian_dataset import HamiltonianDataset, HamiltonianDatabase  # database interface for Hamiltonian datasets
 from .pyg_datasets import PyGNablaDFT, PyGHamiltonianNablaDFT  # PyTorch Geometric interfaces for datasets 
