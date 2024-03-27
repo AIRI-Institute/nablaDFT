a9a76ca2d8156575a4e1d6b1f9264e863f53cabe
BerAnton <ber@airi.net>
2024-03-27 17:30:17 +0300

    add basic example for geometry optimization

--- nablaDFT/optimization/__init__.py
+++ nablaDFT/optimization/__init__.py
@@ -1,3 +1,4 @@
 from .task import BatchwiseOptimizeTask
 from .calculator import PyGBatchwiseCalculator, SpkBatchwiseCalculator
 from .optimizers import ASEBatchwiseLBFGS
+from .pyg_ase_interface import PYGAseInterface
