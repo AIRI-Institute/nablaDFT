382f01790267268bfa96030f88a131ccee470bbf
BerAnton <ber@airi.net>
2024-03-28 16:55:04 +0300

    rework ase task due to ipykernel bug

--- nablaDFT/ase_model/task.py
+++ nablaDFT/ase_model/task.py
@@ -1,11 +1,9 @@
 from typing import Any, Dict, List, Optional, Type
 
-import pytorch_lightning as pl
 import schnetpack as spk
 import torch
 from schnetpack.model.base import AtomisticModel
 from schnetpack.task import UnsupervisedModelOutput
-from torch import nn
 
 
 class AtomisticTaskFixed(spk.task.AtomisticTask):
@@ -21,20 +19,18 @@ class AtomisticTaskFixed(spk.task.AtomisticTask):
         scheduler_monitor: Optional[str] = None,
         warmup_steps: int = 0,
     ):
-        pl.LightningModule.__init__(self)
+        
+        super(AtomisticTaskFixed, self).__init__(
+            model=model,
+            outputs=outputs,
+            optimizer_cls=optimizer_cls,
+            optimizer_args=optimizer_args,
+            scheduler_cls=scheduler_cls,
+            scheduler_args=scheduler_args,
+            scheduler_monitor=scheduler_monitor,
+            warmup_steps=warmup_steps
+        )
         self.model_name = model_name
-        self.model = model
-        self.optimizer_cls = optimizer_cls
-        self.optimizer_kwargs = optimizer_args
-        self.scheduler_cls = scheduler_cls
-        self.scheduler_kwargs = scheduler_args
-        self.schedule_monitor = scheduler_monitor
-        self.outputs = nn.ModuleList(outputs)
-
-        self.grad_enabled = len(self.model.required_derivatives) > 0
-        self.lr = optimizer_args["lr"]
-        self.warmup_steps = warmup_steps
-        self.save_hyperparameters(ignore=["model"])
 
     def predict_step(self, batch, batch_idx):
         torch.set_grad_enabled(self.grad_enabled)
