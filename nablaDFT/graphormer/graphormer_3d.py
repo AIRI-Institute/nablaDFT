922842dbbace7e4db67676e5d460a977631322d2
BerAnton <ber@airi.net>
2024-04-24 11:51:11 +0300

    change graphormer scheduler instantiation

--- nablaDFT/graphormer/graphormer_3d.py
+++ nablaDFT/graphormer/graphormer_3d.py
@@ -12,8 +12,8 @@ import pytorch_lightning as pl
 
 
 @torch.jit.script
-def softmax_dropout(input, dropout_prob: float, is_training: bool):
-    return F.dropout(F.softmax(input, -1), dropout_prob, is_training)
+def softmax_dropout(x, dropout_prob: float, is_training: bool):
+    return F.dropout(F.softmax(x, -1), dropout_prob, is_training)
 
 
 class SelfMultiheadAttention(nn.Module):
@@ -380,9 +380,7 @@ class Graphormer3DLightning(pl.LightningModule):
         y = batch.y
         energy_out, forces_out, mask_out = self(batch)
         loss_energy = self.loss(energy_out, y)
-        forces, mask_forces = to_dense_batch(
-            batch.forces, batch.batch, batch_size=bsz
-        )
+        forces, mask_forces = to_dense_batch(batch.forces, batch.batch, batch_size=bsz)
         masked_forces_out = forces_out * mask_forces.unsqueeze(-1)
         loss_forces = self.loss(masked_forces_out, forces)
         loss = self.loss_forces_coef * loss_forces + self.loss_energy_coef * loss_energy
@@ -511,50 +509,3 @@ class Graphormer3DLightning(pl.LightningModule):
         """Function for batch size infer."""
         bsz = batch.batch.max().detach().item() + 1  # get batch size
         return bsz
-
-
-from functools import partial
-from torch.optim.lr_scheduler import LambdaLR
-
-
-def get_linear_schedule_with_warmup(
-    optimizer, num_warmup_steps, num_training_steps, last_epoch=-1
-):
-    # from https://github.com/huggingface/transformers/blob/v4.31.0/src/transformers/optimization.py#L104
-    """
-    Create a schedule with a learning rate that decreases linearly from the initial lr set in the optimizer to 0, after
-    a warmup period during which it increases linearly from 0 to the initial lr set in the optimizer.
-
-    Args:
-        optimizer ([`~torch.optim.Optimizer`]):
-            The optimizer for which to schedule the learning rate.
-        num_warmup_steps (`int`):
-            The number of steps for the warmup phase.
-        num_training_steps (`int`):
-            The total number of training steps.
-        last_epoch (`int`, *optional*, defaults to -1):
-            The index of the last epoch when resuming training.
-
-    Return:
-        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
-    """
-
-    lr_lambda = partial(
-        _get_linear_schedule_with_warmup_lr_lambda,
-        num_warmup_steps=num_warmup_steps,
-        num_training_steps=num_training_steps,
-    )
-    return LambdaLR(optimizer, lr_lambda, last_epoch)
-
-
-def _get_linear_schedule_with_warmup_lr_lambda(
-    current_step: int, *, num_warmup_steps: int, num_training_steps: int
-):
-    # from https://github.com/huggingface/transformers/blob/v4.31.0/src/transformers/optimization.py#L98
-    if current_step < num_warmup_steps:
-        return float(current_step) / float(max(1, num_warmup_steps))
-    return max(
-        0.0,
-        float(num_training_steps - current_step)
-        / float(max(1, num_training_steps - num_warmup_steps)),
-    )
