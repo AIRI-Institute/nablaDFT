f9a0ef59318ebdd62e1a542a0d21e16327b01f5b
BerAnton <ber@airi.net>
2024-06-14 16:38:06 +0300

    added model checkpoints availability tests

---
+++ tests/download/test_checkpoints.py
@@ -0,0 +1,26 @@
+import os
+import json
+from urllib import request as request
+
+import pytest
+
+import nablaDFT
+from nablaDFT import model_registry
+from nablaDFT.utils import file_validation
+
+
+def read_model_names():
+    path = os.path.join(nablaDFT.__path__[0], "links/models_checkpoints.json")
+    with open(path, "r") as fin:
+        links = json.load(fin)
+    urls = links["checkpoints"].keys()
+    return [pytest.param(url) for url in urls]
+
+
+@pytest.mark.download
+@pytest.mark.parametrize("checkpoint_name", read_model_names())
+def test_model_checkpoints(tmp_path, checkpoint_name):
+    save_path = os.path.join(tmp_path, checkpoint_name)
+    url = model_registry.get_pretrained_model_url(checkpoint_name)
+    request.urlretrieve(url, save_path)
+    assert file_validation(save_path, model_registry.get_pretrained_model_etag(checkpoint_name))
