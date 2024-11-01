5ac3af5a83adbc28ff723a7284b3826c596309fb
Anton Ber <berantonmsu@gmail.com>
2024-11-01 18:44:36 +0300

    temp commit for convert

--- nablaDFT/dataset/convert.py
+++ nablaDFT/dataset/convert.py
@@ -7,21 +7,20 @@ import torch_geometric as pyg
 
 
 @singledispatch
-def convert_nabla_hamiltonian(type_cls, data):
+def convert(type_cls, data):
     raise TypeError(f"Cannot convert {type(data)} to {type_cls}")
 
 
 @convert.register
-def convert_nabla_hamiltonian(type_cls: torch.Tensor, data: Dict[str, np.array]) -> Dict[torch.Tensor]:
+def _(type_cls: torch.Tensor, data: Dict[str, np.array]) -> Dict[torch.Tensor]:
     for key, value in data.items():
-        np_dtype = value.dtype
-        data[key] = torch.from_numpy(value.copy()).to(NUMPY_TO_TORCH[np_dtype])
+        data[key] = torch.from_numpy(value.copy())
     return data
 
 
 @convert.register
-def convert_nabla_hamiltonian(type_cls: pyg.data.Data, data: Dict[np.array]) -> pyg.data.Data:
-    data = convert_nabla_hamiltonian(torch.Tensor, data)
+def _(type_cls: pyg.data.Data, data: Dict[np.array]) -> pyg.data.Data:
+    data = convert(torch.Tensor, data)
     return type_cls(**data)
 
 
