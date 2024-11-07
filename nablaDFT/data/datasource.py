03cc1dd74a303cb696e8b93bc59f89fa31c58a9e
Anton Ber <berantonmsu@gmail.com>
2024-11-07 19:09:16 +0300

    temp commit

--- nablaDFT/data/datasource.py
+++ nablaDFT/data/datasource.py
@@ -17,8 +17,9 @@ Examples:
     }
 """
 
+import math
 import pathlib
-from functools import cached_property
+from ast import literal_eval
 from typing import Dict, List, Optional, Tuple, Union
 
 import apsw  # way faster than sqlite3
@@ -37,7 +38,7 @@ class EnergyDatabase:
 
     Args:
         filepath (pathlib.Path): path to existing database or path for new database.
-        mapping (Dict[str, str]): mapping from column names in database to sample's keys.
+        metadata (Optional[DatasetCard]): optional, dataset metadata.
     """
 
     type = "db"  # only sqlite3 databases
@@ -51,7 +52,8 @@ class EnergyDatabase:
 
         # parse sample schema and metadata
         if metadata is not None:
-            pass
+            self.name = metadata.name
+            self.metadata = metadata.metadata
 
     def __getitem__(self, idx: int) -> Dict[str, np.ndarray]:
         """Returns unpacked element from ASE database.
@@ -77,23 +79,39 @@ class EnergyDatabase:
         """Returns number of rows in database."""
         return len(self._db)
 
-    def write(self, atoms: Atoms) -> None:
-        with self._db as db:
-            db.write(atoms)
+    def write(self, values: Dict[str, np.ndarray]) -> None:
+        # forces and energy stored in data field
+        data_field = {"energy": values.pop("energy"), "forces": values.pop("forces")}
+        atoms = Atoms(data=data_field, **values)
+        self._db.write(atoms)
+
+    @property
+    def metadata(self) -> Dict[str, str]:
+        return str(self.metadata)
+
+    @metadata.setter
+    def metadata(self, metadata: Dict[str, str]) -> None:
+        self.metadata = metadata
 
 
 class SQLite3Database:
     """Database interface for sqlite3 databases.
 
-    Wraps sqlite3 database for training Hamiltonian-like models.
+    Wraps sqlite3 database with square-shaped data, like Hamiltonian and Overlap matrices
+    for model training.
 
     Args:
         filepath (pathlib.Path): path to existing database or path for new database.
+        metadata (Optional[DatasetCard]): dataset metadata.
     """
+
+    type = ".db"
 
     def __init__(self, filepath: Union[pathlib.Path, str], metadata: Optional[DatasetCard] = None) -> None:
         if isinstance(filepath, str):
             filepath = pathlib.Path(filepath)
+        if filepath.suffix != self.type:
+            raise ValueError(f"Invalid file type: {filepath.suffix}")
         self.filepath = filepath.absolute()
         self._connections = {}
 
@@ -130,11 +148,13 @@ class SQLite3Database:
     def _get_connection(self) -> None:
         pass
 
-    @cached_property
+    @property
     def metadata(self) -> Dict[str, str]:
-        cursor = self._get_connection(flags=apsw.SQLITE_OPEN_READONLY).cursor()
-        data = cursor.execute("""SELECT * FROM metadata WHERE id=0""").fetchall()
-        return data
+        return str(self.metadata)
+
+    @metadata.setter
+    def metadata(self, metadata: Dict[str, str]) -> None:
+        self.metadata = metadata
 
 
 def _blob(array: np.ndarray, dtype: np.dtype) -> memoryview:
@@ -187,9 +207,28 @@ def _parse_schema(mapping: Dict[str, str]) -> Tuple[List]:
     return sample_keys, tables, columns
 
 
-def _parse_dtypes(dtypes_dict: Dict[str, str]) -> Dict[str, np.dtype]:
-    pass
+def _parse_dtypes(dtypes: Dict[str, str]) -> Dict[str, np.dtype]:
+    dtypes = {}
+    for key, value in dtypes.items():
+        dtypes[key] = np.dtype(value)
+    return dtypes
 
 
-def _parse_shapes(dtypes_dict: Dict[str, str]) -> Dict[str, tuple]:
-    pass
+def _parse_shapes(shapes: Dict[str, str]) -> Dict[str, tuple]:
+    shapes = {}
+    for key, value in shapes.items():
+        shapes[key] = literal_eval(value)
+    return shapes
+
+
+def _matrix_from_bytes(buf: memoryview, dtype: np.dtype) -> np.ndarray:
+    """Helper function creates numpy square matrix from bytes.
+
+    Shape of matrix infered from buffer size and dtype.
+
+    Args:
+        buf (memoryview): buffer object to convert.
+        dtype (np.dtype): dtype of array.
+    """
+    elem_num = math.sqrt(len(buf) / dtype.itemsize)
+    return _deblob(buf=buf, dtype=dtype, shape=(elem_num, elem_num))
